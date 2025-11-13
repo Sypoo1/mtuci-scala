eaf491085533e92e655ccd43fd1e5248178ffc78
Rustam Shamsutdinov <sypooit@gmail.com>
2025-11-13 05:20:55 +0300

    done 3

--- lab3/patternmatching/src/main/scala/Main.scala
+++ lab3/patternmatching/src/main/scala/Main.scala
@@ -41,5 +41,39 @@ object PatternMatching {
     val examples: List[Any] =
       List(4, 7, "", "Scala", people.head, List(1, 2, 3), Some(42), None, 3.14)
     examples.foreach(e => println(s"$e => ${{ handleValue(e) }}"))
+
+    println("\ntask 3:\n")
+
+    sealed trait Expr
+    case class Number(value: Int) extends Expr
+    case class Add(a: Expr, b: Expr) extends Expr
+    case class Mul(a: Expr, b: Expr) extends Expr
+
+    def eval(expr: Expr): Int = expr match {
+      case Number(v) => v
+      case Add(a, b) => eval(a) + eval(b)
+      case Mul(a, b) => eval(a) * eval(b)
+    }
+
+    val expr1 = Add(Number(2), Number(3))
+    val expr2 = Mul(Add(Number(3), Number(4)), Number(4))
+
+    println(s"expr1 = $expr1 => result = ${eval(expr1)}")
+    println(s"expr2 = $expr2 => result = ${eval(expr2)}")
+
+    println("\ntask 4:\n")
+
+    def identity[A](value: A): A = value
+    def swap[A, B](pair: (A, B)): (B, A) = (pair._2, pair._1)
+    def filterWith[A](seq: Seq[A], predicate: A => Boolean): Seq[A] =
+      seq.filter(predicate)
+
+    println(identity(42))
+    println(identity("hello"))
+
+    println(swap("Alice", 30))
+
+    println(filterWith(List(1, 2, 3, 4, 5, 6), x => x % 2 == 0))
+    println(filterWith(List("Bob", "Dean", "Mark", "Rob"), s => s.length % 2 == 0))
   }
 }
