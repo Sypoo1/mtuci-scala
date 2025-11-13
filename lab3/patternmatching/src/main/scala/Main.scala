a38652ff6f7c3c98d67b1b584073ac833f0ecb70
Rustam Shamsutdinov <sypooit@gmail.com>
2025-11-13 04:51:11 +0300

    done 3.2

--- lab3/patternmatching/src/main/scala/Main.scala
+++ lab3/patternmatching/src/main/scala/Main.scala
@@ -1,8 +1,8 @@
+case class Person(name: String, age: Int, city: Option[String])
+
 object PatternMatching {
   def main(args: Array[String]): Unit = {
     println("\ntask 1:\n")
-
-    case class Person(name: String, age: Int, city: Option[String])
 
     val people = List(
       Person("Joe", 21, Some("Los Angeles")),
@@ -26,5 +26,20 @@ object PatternMatching {
 
     println("\ntask 2:\n")
 
+    def handleValue(x: Any): String = x match {
+      case n: Int if n % 2 == 0     => s"Even Int: $n"
+      case n: Int                   => s"Odd Int: $n"
+      case s: String if s.isEmpty() => s"Empty string"
+      case s: String                => s"String: $s"
+      case p: Person                => s"Person: ${p.name}, ${p.age} y.o."
+      case l: List[_]               => s"List of size ${l.size}"
+      case Some(v)                  => s"Option with $v"
+      case None                     => "None"
+      case _                        => "Unknown type"
+    }
+
+    val examples: List[Any] =
+      List(4, 7, "", "Scala", people.head, List(1, 2, 3), Some(42), None, 3.14)
+    examples.foreach(e => println(s"$e => ${{ handleValue(e) }}"))
   }
 }
