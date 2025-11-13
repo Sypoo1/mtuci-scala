5bffa09307645f146e93cad0b3b60d15c325740b
Rustam Shamsutdinov <sypooit@gmail.com>
2025-11-13 04:33:58 +0300

    done 3.1

--- lab3/patternmatching/src/main/scala/Main.scala
+++ lab3/patternmatching/src/main/scala/Main.scala
@@ -1,5 +1,30 @@
-@main def hello(): Unit =
-  println("Hello world!")
-  println(msg)
+object PatternMatching {
+  def main(args: Array[String]): Unit = {
+    println("\ntask 1:\n")
 
-def msg = "I was compiled by Scala 3. :)"
+    case class Person(name: String, age: Int, city: Option[String])
+
+    val people = List(
+      Person("Joe", 21, Some("Los Angeles")),
+      Person("Alice", 25, None),
+      Person("Bryan", 16, Some("Austin")),
+      Person("David", 15, None)
+    )
+
+    def describe(p: Person): String = p match {
+      case Person(name, age, Some(city)) if age >= 18 =>
+        s"$name, $age y.o. lives in $city, adult"
+      case Person(name, age, Some(city)) if age < 18 =>
+        s"$name, $age y.o. lives in $city, kid"
+      case Person(name, age, None) if age >= 18 =>
+        s"$name, $age y.o. homeless adult"
+      case Person(name, age, None) if age < 18 =>
+        s"$name, $age y.o. homeless kid"
+      case _ => "no data"
+    }
+    people.map(describe).foreach(println)
+
+    println("\ntask 2:\n")
+
+  }
+}
