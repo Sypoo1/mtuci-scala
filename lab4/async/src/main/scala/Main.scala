325bd68620a73ad8b57e44d3edd73a1b0f840786
Sypoo <sypooit@gmail.com>
2025-11-27 03:44:07 +0300

    done 4

--- lab4/async/src/main/scala/Main.scala
+++ lab4/async/src/main/scala/Main.scala
@@ -1,27 +1,54 @@
 import scala.concurrent._
-import scala.concurrent.duration._
 import ExecutionContext.Implicits.global
+import scala.concurrent.duration._
 import scala.util.{Success, Failure}
 
 object AsyncLab {
-  def main(args: Array[String]): Unit = {
 
+  def runTask1(): Unit = {
     println("\ntask 1:\n")
 
     val futureResult = Future {
-      println("Start async computation")
+      println("Task 1 - Async computation started")
       Thread.sleep(2000)
-      println("Async computation completed")
+      println("Task 1 - Async computation finished")
       42
     }
 
-    futureResult.onComplete {
-        case Success(value) => println(s"Result: $value")
-        case Failure(exception) => println(s"Error: ${exception.getMessage}")
+    val result = Await.result(futureResult, 3.seconds)
+    println(s"Task 1 - Result: $result")
+    println("Task 1 - Completed")
+  }
+
+  def runTask2(): Unit = {
+    println("\ntask 2:\n")
+
+    val promise = Promise[Int]()
+    val promiseFuture = promise.future
+
+    Future {
+      println("Task 2 - Separate thread computation started")
+      try {
+        val a = 26
+        val b = 2
+        Thread.sleep(1000)
+        val result = a * b
+        promise.success(result)
+        println("Task 2 - Computation finished, promise fulfilled")
+      } catch {
+        case e: Exception =>
+          promise.failure(e)
+          println("Task 2 - Error during computation, promise failed")
+      }
     }
 
-    println("Waiting for result")
-    Await.result(futureResult, 3.seconds)
-    println("task 1 is done")
+    val finalResult = Await.result(promiseFuture, 3.seconds)
+    println(s"Task 2 - Promise future result: $finalResult")
+    println("Task 2 - Completed")
+  }
+
+  def main(args: Array[String]): Unit = {
+    runTask1()
+    runTask2()
   }
 }
